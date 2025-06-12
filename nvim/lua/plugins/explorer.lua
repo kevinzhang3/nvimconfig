e54acf031e9945c3ee9797ade00004b5220e0d44
Kevin <kzhang366@gmail.com>
2025-06-12 14:54:16 -0600

    blink and lsp stuff

--- nvim/lua/plugins/explorer.lua
+++ nvim/lua/plugins/explorer.lua
@@ -19,10 +19,6 @@ return {
 
     case_insensitive = false,
 
-    -- Optional dependencies
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
   }
 } 
